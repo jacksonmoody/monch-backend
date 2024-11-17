eb67c2011608e32c60d54d160ff6a0a78a6e5965
josh <josh@joshs-MacBook-Pro-5.local>
2024-11-17 02:49:38 -0600

    maybe

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask
+from tensorflow.keras.models import load_model
+import numpy as np
+from tensorflow.keras.preprocessing import image
+import json
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def main():
+    model = load_model('food101_classifier.h5')
+    with open('monch-backend/food_nums.json', 'r') as f:
+        food_nums = json.load(f)
+
+    img_path = '../imgs/hotdog.jpg' # replace with database query or something
+    img = image.load_img(img_path, target_size=(128, 128))
+    img_array = image.img_to_array(img) 
+    img_array = np.expand_dims(img_array, axis=0) 
+    img_array = img_array * (1 / 255.0) 
+
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction)
+    print(food_nums[str(predicted_class)])
     return 'Hello, World!'
