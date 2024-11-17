d1e9242d1f7e464bbd61099d9873421e25c44505
josh <josh@joshs-MacBook-Pro-5.local>
2024-11-17 09:41:22 -0600

    josh adding some files

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    model = load_model('food101_classifier.h5')
-    with open('monch-backend/food_nums.json', 'r') as f:
-        food_nums = json.load(f)
+    # model = load_model('food101_classifier.h5')
+    # with open('monch-backend/food_nums.json', 'r') as f:
+    #     food_nums = json.load(f)
 
-    img_path = '../imgs/hotdog.jpg' # replace with database query or something
-    img = image.load_img(img_path, target_size=(128, 128))
-    img_array = image.img_to_array(img) 
-    img_array = np.expand_dims(img_array, axis=0) 
-    img_array = img_array * (1 / 255.0) 
+    # img_path = '../imgs/hotdog.jpg' # replace with database query or something
+    # img = image.load_img(img_path, target_size=(128, 128))
+    # img_array = image.img_to_array(img) 
+    # img_array = np.expand_dims(img_array, axis=0) 
+    # img_array = img_array * (1 / 255.0) 
 
-    prediction = model.predict(img_array)
-    predicted_class = np.argmax(prediction)
-    print(food_nums[str(predicted_class)])
+    # prediction = model.predict(img_array)
+    # predicted_class = np.argmax(prediction)
+    # print(food_nums[str(predicted_class)])
     return 'Hello, World!'
